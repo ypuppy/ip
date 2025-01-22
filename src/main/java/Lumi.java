442be32360999e04874495804203c473c6ee1744
ypuppy <>
2025-01-22 14:41:50 +0800

    added a List

--- src/main/java/Lumi.java
+++ src/main/java/Lumi.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Lumi {
     public static void main(String[] args) {
@@ -13,9 +14,8 @@ public class Lumi {
         System.out.println("Hello! I'm Lumi");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
 
+        ArrayList<String> tasks = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         String input;
 
@@ -26,9 +26,20 @@ public class Lumi {
                 System.out.println("    Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break; // Exit the loop
-            } else {
+            } else if (input.equals("list")) {
                 System.out.println("____________________________________________________________");
-                System.out.println("    " + input); // Echo the user's input
+                if (tasks.isEmpty()) {
+                    System.out.println("    No tasks added yet!");
+                } else {
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println((i + 1) + ". " + tasks.get(i)); // Display stored inputs
+                    }
+                }
+                continue;
+            } else {
+                tasks.add(input); // Add input to the list
+                System.out.println("____________________________________________________________");
+                System.out.println("    added: " + input);
                 System.out.println("____________________________________________________________");
             }
         }
